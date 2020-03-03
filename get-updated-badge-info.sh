d16756aef2c685836534a1b2d295f7efb84af3fd
OkyHp <ivanets_99@mail.ru>
2020-03-03 05:19:47 +0200

    Autizm #3

--- get-updated-badge-info.sh
+++ get-updated-badge-info.sh
@@ -4,4 +4,4 @@ echo "collecting stas for badges"
 
 commits=`git rev-list --all --count`
 latest_release_tag=`git describe --abbrev=0`
-echo "{\"commits\":\"$commits\", \"release_tag"\:\"$latest_release_tag\"}" > badges.json
+echo "{\"commits\":\"$commits\", \"release_tag\":\"$latest_release_tag\"}" > badges.json
